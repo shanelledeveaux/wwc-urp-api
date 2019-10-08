d24d36e66e7a25da122ef21b1d36f2799b36632a
khamwas <eebravo1@gmail.com>
2019-10-07 19:44:19 -0500

    updated migrations

--- Controllers/UrpController.cs
+++ Controllers/UrpController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using TodoApi.Models;
 using WwcUrpApi.Models;
 
 namespace WwcUrpApi.Controllers
---
+++ Migrations/20191008003743_initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace wwcurpapi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pronoun",
+                columns: table => new
+                {
+                    PronounId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Subject = table.Column<string>(nullable: true),
+                    Object = table.Column<string>(nullable: true),
+                    Posessive = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pronoun", x => x.PronounId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FullName = table.Column<string>(nullable: true),
+                    CommonName = table.Column<string>(nullable: true),
+                    BirthYear = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    PronounId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_People_Pronoun_PronounId",
+                        column: x => x.PronounId,
+                        principalTable: "Pronoun",
+                        principalColumn: "PronounId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_PronounId",
+                table: "People",
+                column: "PronounId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Pronoun");
+        }
+    }
+}
